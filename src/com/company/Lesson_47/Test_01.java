61c38cdc7bb931f13323e2b9eada5a81ef62cf27
VasylN <vasyln86@gmail.com>
2017-03-19 17:00:40 +0200

    Add_new_task_02

--- src/com/company/Lesson_47/Test_01.java
+++ src/com/company/Lesson_47/Test_01.java
@@ -1,7 +1,25 @@
 package com.company.Lesson_47;
 
+import java.util.Arrays;
+
 /**
  * Created by Pc on 19.03.2017.
  */
 public class Test_01 {
+    public static void main(String[] args) {
+        int[] mas = {1, 2, 3, 1, 2};
+        System.out.println(containsDuplicates(mas));
+    }
+
+    public static boolean containsDuplicates(int[] a) {
+        Arrays.sort(a);
+        for (int i = 0; i < a.length - 1 ; i++) {
+            if (a[i] == a[i + 1]) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
 }
