52f46babded92d1e08bfff3b7f104baa01fb4176
VasylN <vasyln86@gmail.com>
2017-03-22 19:23:03 +0200

    Add_new_task_03

---
+++ src/com/company/Lesson_47/Test_03.java
@@ -0,0 +1,17 @@
+package com.company.Lesson_47;
+
+/**
+ * Created by Pc on 22.03.2017.
+ */
+public class Test_03 {
+    public static void main(String[] args) {
+        String s1 = new String("sds");
+        String s2 = new String("sds");
+        if(s1 == s2){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+
+}
