6b90b0a53ab3de9ce47f3d25c6c45410822101de
VasylN <vasyln86@gmail.com>
2017-04-11 00:03:46 +0300

    Add_new_task_14

---
+++ src/com/company/HomWork/Repeat/Test_10_04.java
@@ -0,0 +1,33 @@
+package com.company.HomWork.Repeat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Pc on 10.04.2017.
+ *//* Самая длинная строка
+ найди самую длинную строку в списке.
+*/
+public class Test_10_04 {
+    public static void main(String[] args) {
+
+    }
+    public static List<String> createList(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("SSS");
+        stringList.add("SSSee");
+        stringList.add("SSSeeee");
+        stringList.add("SSSeeeeee");
+        for (int i = 0; i < stringList.size(); i++) {
+
+        }
+        return stringList;
+    }
+    public static List<String> maxString(){
+        int max = 0;
+        for (int i = 0; i < s; i++) {
+
+        }
+
+    }
+}
